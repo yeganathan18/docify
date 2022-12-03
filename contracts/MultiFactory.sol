2cd05fb3ddf275042294038991b58699b35d2447
Pranavvks <pranav.vks@gmail.com>
2022-12-04 00:49:18 +0530

    MultiSig SmartContract Setup

---
+++ contracts/MultiFactory.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "./MultiSigWallet.sol";
+
+contract MultiFactory{
+    // factory contract onwer
+    address private immutable multiSigFactoryOwner;
+
+    // struct to store all the data of MultiSigWallet and MultiFactory contract
+    struct multiFactoryStruct {
+        address[] onwers;
+        uint required;
+        address multiSigFactoryOwner;
+    }
+
+    // searching the struct data of MultiSigWallet and MultiSigWallet factory using owner address
+    mapping(address => multiFactoryStruct) public allMultiSigWallet;
+
+    // owner address, onwer address will be used check which address own/create a new multisg wallet
+    mapping(address => address) public searchByAddress;
+
+    // number of MultiSigWallet created
+    uint256 public numOfMultiSigWallet;
+
+    /**
+     * @dev constructor to get the owner address of this contract factory
+     */
+    constructor(address _multiSigFactoryOwner) {
+        multiSigFactoryOwner = _multiSigFactoryOwner;
+    }
+
+    /**
+     * @dev function to create the contract MultiSigWallet
+     */
+    function createMultiSigWallet(address[] memory _onwers, uint _required) public {
+        // Create a new Wallet contract
+        MultiSigWallet multiSigWallet =  new MultiSigWallet(
+            _onwers,
+            _required
+        );
+        // Increment the number of MultiSigWallet
+        numOfMultiSigWallet++;
+
+        // Add the new MultiSigWallet to the mapping
+        allMultiSigWallet[msg.sender] = (
+            multiFactoryStruct(
+                _onwers,
+                _required,
+                address(this)
+            )
+        );
+
+        // search the profile by using owner address
+        searchByAddress[msg.sender] = address(multiSigWallet);
+    }
+
+    // function to withdraw the fund from contract factory
+    function withdraw(uint256 amount) external payable {
+        require(msg.sender == multiSigFactoryOwner, "ONLY_ONWER_CAN_CALL_FUNCTION");
+        // sending money to contract owner
+        (bool success, ) = multiSigFactoryOwner.call{value: amount}("");
+        require(success, "TRANSFER_FAILED");
+    }
+
+    // get the balance of the contract
+    function getContractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    // get the address of this contract
+    function getAddressOfContract() public view returns (address) {
+        return address(this);
+    }
+
+    // get the address of trustFactory contract owner
+    function getAddressOfMultiSigFactoryOwner() public view returns (address) {
+        return multiSigFactoryOwner;
+    }
+
+    // receive function is used to receive Ether when msg.data is empty
+    receive() external payable {}
+
+    // Fallback function is used to receive Ether when msg.data is NOT empty
+    fallback() external payable {}
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("hardhat-deploy");
+require("@nomiclabs/hardhat-ethers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const GOERLI_RPC_URL = process.env.REACT_APP_ALCHEMY_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+
 module.exports = {
-  solidity: "0.8.17",
-};
+  solidity: "0.8.9" ,
+  defaultNetwork: "goerli",
+     networks: {
+        hardhat: {},
+        goerli: { 
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId : 5
+           },
+       },
+  };
